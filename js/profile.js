4c07f671b5105d02df7c1ad3907d952da1183e23
darothub <darotudeen@gmail.com>
2018-09-16 20:04:14 +0100

    profile page

--- js/profile.js
+++ js/profile.js
@@ -1,7 +1,21 @@
+let arrow = document.getElementById("arrow")
+
+arrow.addEventListener('click', (e)=>{
+    
+    let sidebar = document.getElementById("sidebar")
+
+    if(sidebar.style.display === "block"){
+        sidebar.style.display = "none"
+        return false
+    }
+    sidebar.style.display = "block";
+    e.preventDefault()
+});
+
 const myChecked=()=> {
     let checkBox = document.getElementsByClassName("myCheck");
     let text = document.getElementsByClassName("text");
-    for(let i=0; i<=checkBox.length; i++){
+    for(let i=0; i<checkBox.length; i++){
         if (checkBox[i].checked === true){
             text[i].style.display = "block";
           } else {
