b5396c717db55625507d30fdb9f9a95b69d317a1
darothub <darotudeen@gmail.com>
2018-10-01 12:11:07 +0100

    add signup and signin

--- Server/router/index.js
+++ Server/router/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 import controllers from '../controller/index';
 
+import User from '../controller/auth/user';
+
 import getAll from '../dbController';
 
 const router = express.Router();
@@ -17,5 +19,9 @@ router.put('/api/v1/orders/:id', controllers.updateOrder);
 router.delete('/api/v1/orders/:id', controllers.deleteOrder);
 
 router.get('/api/v1/others', getAll);
+
+router.post('/auth/signup', User.signup);
+
+router.post('/auth/signin', User.signin);
 
 export default router;
