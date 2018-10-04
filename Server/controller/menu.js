a88f78cede513c43d7244ab9deb2cc0104cccbb3
darothub <darotudeen@gmail.com>
2018-10-04 11:45:40 +0100

    add new menu

--- Server/controller/menu.js
+++ Server/controller/menu.js
@@ -1,4 +1,6 @@
+import jwt from 'jsonwebtoken';
 import pool from '../model/database';
+import config from '../model/config';
 
 const getAllmenu = (req, res) => {
   const reqQuery = {
@@ -14,4 +16,28 @@ const getAllmenu = (req, res) => {
     .catch(err => res.status(500).send({ message: err.message }));
 };
 
-export default getAllmenu;
+const addNewMenu = (req, res) => {
+  const decoded = jwt.verify(req.token, config.secretkey);
+  const reqQuery = {
+    text: 'INSERT INTO menu(food, price, food_image) VALUES($1, $2, $3) RETURNING *',
+    values: [req.body.food, req.body.price, req.body.image],
+  };
+  if (!req.body.food) {
+    return res.status(400).send({ message: 'food name is required' });
+  }
+  if (!req.body.price) {
+    return res.status(400).send({ message: 'Price is required' });
+  }
+  if (!req.body.image) {
+    return res.status(400).send({ message: 'Food image is essential' });
+  }
+  return pool.query(reqQuery)
+    .then((menu) => {
+      if (decoded.roles !== 'Admin') {
+        return res.status(403).send('Admin not found');
+      }
+      return res.status(200).send({ message: 'Your menu post is successful', data: menu.rows[0], decoded });
+    });
+};
+
+export default { getAllmenu, addNewMenu };
