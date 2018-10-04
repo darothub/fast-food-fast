1982b3c6864d588410fb17049d49db62930ceb27
darothub <darotudeen@gmail.com>
2018-10-04 01:43:19 +0100

    get specific order

--- Server/router/index.js
+++ Server/router/index.js
@@ -2,12 +2,6 @@ import express from 'express';
 
 import controllers from '../controller/index';
 
-import User from '../controller/auth';
-
-import users from '../controller/order';
-
-import checkAuth from '../controller/token/verifytoken';
-
 const router = express.Router();
 
 router.get('/api/v1/orders', controllers.getAllOrders);
@@ -20,14 +14,5 @@ router.put('/api/v1/orders/:id', controllers.updateOrder);
 
 router.delete('/api/v1/orders/:id', controllers.deleteOrder);
 
-router.post('/auth/signup', User.signup);
-
-router.post('/auth/signin', User.signin);
-
-router.post('/orders', checkAuth, users.placeOrder);
-
-router.get('/users/:id/orders', checkAuth, users.getUserOrderHist);
-
-router.get('/orders', checkAuth, users.getAllorders);
 
 export default router;
